3d525c0ae63997342492b22ac57c04de75e4508e
Jean-Marie Lemetayer <j.lemetayer@kerlink.fr>
2022-05-09 12:02:15 +0200

    pre-commit: enforce type hints

--- tools/debug_helpers.py
+++ tools/debug_helpers.py
@@ -2,18 +2,20 @@
 import functools
 import inspect
 import logging
+from typing import Any
+from typing import Callable
 
 logger = logging.getLogger(__name__)
 
 
-def log_function_name(function):
+def log_function_name(function: Callable[..., Any]) -> Callable[..., Any]:
     """Allow to log the function name before each call.
 
     This is a decorator function.
     """
 
     @functools.wraps(function)
-    def log_and_call_function(*args, **kwargs):
+    def log_and_call_function(*args: Any, **kwargs: Any) -> Any:
         stack_level = len(
             [x for x in inspect.stack() if x[3] != "log_and_call_function"]
         )
